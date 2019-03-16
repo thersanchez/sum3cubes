8039db053eae1a90beed89e11c84af4fb8832793
Esther Sánchez <sanchez.ther@gmail.com>
2019-03-16 12:23:39 +0100

    gitignore & dummy cmd

---
+++ cmd/sum3cubes/main.go
@@ -0,0 +1,16 @@
+/*
+	sum3cubes calculates the sum of the cubes of 3 integers.
+    Copyright (C) 2019 Esther Sánchez Aguilar.
+
+	There is a copy of the GNU General Public License in the LICENSE
+	file at the root of this repository. If not, see
+	<https://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("TO DO")
+}
